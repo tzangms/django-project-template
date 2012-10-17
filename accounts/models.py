e1560f76d5e7f65c164403a25821581656cf3508
tzangms <tzangms@gmail.com>
2012-10-17 18:59:56 +0000

    add Profile create signal

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from userena.models import UserenaBaseProfile
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(UserenaBaseProfile):
     user = models.OneToOneField(User, unique=True)
     nickname = models.CharField(max_length=50)
+
+@receiver(post_save, sender=User)
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user = kwargs['instance']
+        Profile(user=user, nickname=user.username).save()
